8f4dbdbf45bce79b14af559b3a96e06203ed3afd
Dinesh Rout <rddinesh779@gmail.com>
2022-01-19 11:40:06 +0530

    applied middlewares

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const userRoute = require('./routes/users');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middlewares
+app.use(express.json());
+
 connectDB();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Ecomm api');
-});
+app.use('/api/users', userRoute);
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
