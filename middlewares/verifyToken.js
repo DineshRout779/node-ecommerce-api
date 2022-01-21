41d64e6d4ec6bbcd5d23d5616d6ec3637a37ba6b
Dinesh Rout <rddinesh779@gmail.com>
2022-01-21 13:45:25 +0530

    added middlewares for jwt token verification

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+      return res.status(400).json({
+        success: false,
+        error: 'No token found!',
+      });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).json({
+          success: false,
+          error: 'Token not valid!',
+        });
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json({
+      success: false,
+      error: 'You are not authorized',
+    });
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user._id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(403).json({
+        status: false,
+        error: 'Unauthorized, Access Denied!',
+      });
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
